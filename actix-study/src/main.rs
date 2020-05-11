2160c954a86c13d826913243c5ceaf79cdc5ab2f
dollarkiller <wangy@mvalley.io>
2020-05-11 13:35:05 +0800

    add actix

--- actix-study/src/main.rs
+++ actix-study/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get,web,App,HttpResponse,HttpServer,Responder};
+use std::thread;
+use std::time::Duration;
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World !!!")
+}
+
+async fn hello() -> impl Responder {
+    thread::sleep(Duration::from_secs(3));
+    HttpResponse::Ok().body("Hello Rust")
+}
+
+#[get("/hello")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||{
+        App::new()
+            .route("/",web::get().to(index))
+            .route("/hello",web::get().to(hello))
+            .service(ping)
+    })
+        .bind("0.0.0.0:8077")?
+        .run()
+        .await
 }
